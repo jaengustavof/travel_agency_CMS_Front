3053db3c2fbfae56cb490c14fba00a7f938cb525
jaengustavof <jaengustavof@gmail.com>
2022-10-15 23:52:50 +0200

    feat: fligth form request

--- src/components/search-forms/flights.jsx
+++ src/components/search-forms/flights.jsx
@@ -3,35 +3,113 @@ import './searchforms.scss'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlane } from '@fortawesome/free-solid-svg-icons';
 import { faPeopleGroup } from "@fortawesome/free-solid-svg-icons";
+import { useState, useEffect, useContext} from 'react';
+import Alert from 'react-bootstrap/Alert';
+import axios from "axios";
+import Context from '../../context';
 
+const handleChange = (e) =>{
+    const returnDate = document.getElementById('return');
+    returnDate.setAttribute('min', e.target.value);
+}
 
 const App = () => {
-    const { register, handleSubmit } = useForm();
-    const onSubmit = data => console.log(data);
+    const { register, handleSubmit, formState: { errors, isSubmitSuccessful }, getValues, reset } = useForm();
+    const today = new Date().toISOString().split('T')[0];
+    const [errorType, setErrorType] = useState(0);
+    let {amadeusToken} = useContext(Context);
+
+    useEffect(()=>{
+        setErrorType(0)
+    },[])
+
+
+    const AlertMessage = ()  => {
+        if(errorType === 1) {
+            return(
+                <Alert variant='danger' className='w-100 text-center'>
+                    Error, Please check dates.
+                </Alert>
+            )
+        }else if(errorType === 0){
+            return
+        }
+    }
+
+    const onSubmit = data => {
+
+        if(data.departure > data.arrival){
+            setErrorType(1)
+            return;
+        }else if(data.departure <= data.arrival){
+            setErrorType(0);
+            let { origin, destination, departure, arrival, passengers} = data;
+            origin = origin.toUpperCase();
+            destination = destination.toUpperCase();
+
+            let config = {
+                method: 'get',
+                url: `https://test.api.amadeus.com/v2/shopping/flight-offers?originLocationCode=${origin}&destinationLocationCode=${destination}&departureDate=${departure}&returnDate=${arrival}&adults=${passengers}&max=5`,
+                headers: {  Authorization: `Bearer ${amadeusToken}` }
+              };
+              console.log('waiting')
+              axios(config)
+              .then(function (response) {
+                console.log(response.data);
+              })
+              .catch(function (error) {
+                console.log(error);
+              });
+        }        
+
+    }
+
+    
+
     return (
         <form onSubmit={handleSubmit(onSubmit)} className='request-form'>
-            <fieldset className='fieldset-container d-flex flex-row justify-content-between align-items-end flex-wrap pt-5'>
+            <fieldset className='fieldset-container d-flex flex-row justify-content-between align-items-start flex-wrap pt-5'>
+
                 <div className="inputContainer">
-                    <input {...register("origin")} placeholder='Origin' className='city'/>
+
+                    <input {...register("origin", {required:'Origin is required'})} placeholder='Origin' className='city'/>
+
+                    {errors.origin && <span>{errors.origin.message}</span>}
+
                     <FontAwesomeIcon icon={faPlane} className='input-icon' />
-                </div>
-                <div className="inputContainer">
-                    <input {...register("destination")} placeholder='Destination' className='city'/>
-                    <FontAwesomeIcon icon={faPlane} className='input-icon' />
+
                 </div>
 
                 <div className="inputContainer">
-                    <input type="date" {...register("departure")} className='mt-4'/>
+
+                    <input {...register("destination", {required:'Destination is required'})} placeholder='Destination' className='city'/>
+
+                    {errors.destination && <span>{errors.destination.message}</span>}
+
+                    <FontAwesomeIcon icon={faPlane} className='input-icon' />
+
+                </div>
+
+                <div className="inputContainer">
+
+                    <input type="date" {...register("departure", {required: 'Departure date required'})} className='mt-4' onChange={handleChange} min={today} />
+
+                    {errors.departure && <span>{errors.departure.message}</span>}
+
                 </div>
                 
                 <div className="inputContainer">
-                    <input type="date"{...register("arrival")} className='mt-4'/>
-                </div>
-            </fieldset>
 
+                    <input type="date"{...register("arrival")} className='mt-4 ' id="return" min={today}/>
+
+                </div>
+
+            </fieldset>
+            
             <fieldset className='d-flex flex-row justify-content-between align-items-end flex-wrap pt-5'>
                 <div className="inputContainer">  
-                    <select {...register("passenges")}>
+
+                    <select {...register("passengers")}>
                         <option value="" disabled>Passengers</option>
                         <option value="1">1</option>
                         <option value="2">2</option>
@@ -43,12 +121,17 @@ const App = () => {
                         <option value="8">8</option>
                         <option value="9">9</option>
                     </select>
+
                     <FontAwesomeIcon icon={faPeopleGroup} className='input-icon' />
+
                 </div> 
+                
             </fieldset>
 
             <fieldset className='d-flex flex-row justify-content-between flex-wrap pt-5'>
+            <AlertMessage/>
                 <input type="submit" className="confirm-button" value="Search Flights"/>
+
             </fieldset>
         </form>
     )
--- src/index.js
+++ src/index.js
@@ -12,22 +12,58 @@ import LogForm from './pages/logIn';
 import Users from './pages/user'
 import Header from "./components/header/Header";
 import Context from './context';
+import axios from "axios";
 import { useState, useEffect } from 'react';
-import amadeusAuth from "./hooks/amadeusAuth";
+//import amadeusAuth from "./hooks/amadeusAuth";
+import qs from "qs";
+
+
 
 const Container = () => {
-  useEffect(()=>{
-    amadeusAuth();
-    setInterval(() => {
-      console.log('test')
-    }, 1000);
-  })
 
   
   const [users, setUsers] = useState();
   const [amadeusToken, setAmadeusToken] = useState('')
   const [logedUsers, setlogedUsers] = useState('');
-  const [regStep, setRegStep] = useState(1)
+  const [regStep, setRegStep] = useState(1);
+
+  const amadeusAuth = async() => {
+
+    const data = qs.stringify({
+      'client_id': `hLTNNmBLAAry5gQ2xLKAZxEBJ9QN2Ux5`,
+      'client_secret': `kMk70AxbxzFUbcVb`,
+      'grant_type': 'client_credentials' 
+      });
+  
+      const config = {
+          method: 'post',
+          url: 'https://test.api.amadeus.com/v1/security/oauth2/token',
+          headers: { },
+          data : data
+        };
+  
+      axios(config)
+      .then(function (response) {
+        setAmadeusToken(response.data.access_token
+          );
+      
+      })
+      .catch(function (error) {
+      console.log(error);
+      });
+  }
+
+  useEffect(()=>{
+    amadeusAuth()    
+    setInterval(() => {
+      amadeusAuth() 
+      console.log('cambia despues de 25min', amadeusToken)
+    }, 1000 * 60 * 25);
+  },[])
+  
+  console.log(amadeusToken)
+ 
+  
   return (
     
     <Context.Provider value={{users, setUsers, logedUsers, setlogedUsers, regStep, setRegStep, amadeusToken, setAmadeusToken}}>
